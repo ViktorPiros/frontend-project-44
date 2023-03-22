d7c935fac1033739bf8bac873efb1d6e2934b9f8
VP <viktorpiros4@mail.ru>
2023-03-22 12:45:17 +0300

    put readline and made a player greeting

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,7 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
+import getUserName from '../src/cli.js'
+let userName
+function name() {
+    const userName = getUserName();
+};
+userName = name
---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+console.log('Welcome to the Brain Games!');
+const getUserName = {
+    const: readlineSync.question('May I have your name? ')
+}
+export default getUserName

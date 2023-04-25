077c561db6e3d3a20b016ef1a19e717d6ed4f63e
VP <viktorpiros4@mail.ru>
2023-04-25 14:23:02 +0300

    test commit

--- src/games/brain-even-game.js
+++ src/games/brain-even-game.js
@@ -3,3 +3,8 @@ import getRandomIntInclusive from '../utils/randomNumber.js';
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 const numb = getRandomIntInclusive();
 console.log(`Question: ${numb}`);
+function rightAnswerParity() {
+  const rightAnswer = numb % 2 === 0 ? 'yes' : 'no';
+    
+}
+console.log(`Your answer: ${rightAnswerParity}`);

c3ed1f901ed5ebb691539d8410d578c6b4fdf235
VP <viktorpiros4@mail.ru>
2023-04-27 13:32:52 +0300

    brain-even-games.js it is worked

--- src/games/brain-even-game.js
+++ src/games/brain-even-game.js
@@ -1,10 +1,23 @@
+import readlineSync from 'readline-sync';
 import getRandomIntInclusive from '../utils/randomNumber.js';
 
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-const numb = getRandomIntInclusive();
-console.log(`Question: ${numb}`);
-function rightAnswerParity() {
-  const rightAnswer = numb % 2 === 0 ? 'yes' : 'no';
-    
+function rightAnswerParity(number) {
+  const rightAnswer = number % 2 === 0 ? 'yes' : 'no';
+  return rightAnswer;
 }
-console.log(`Your answer: ${rightAnswerParity}`);
+
+function even() {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const numb = getRandomIntInclusive();
+  console.log(`Question: ${numb}`);
+
+  const correctAnswer = rightAnswerParity(numb);
+  const usrAnswer = readlineSync.question('Your answer: ');
+  if (usrAnswer === correctAnswer) {
+    console.log('Correct');
+  } else {
+    console.log('Baka');
+  }
+}
+
+even();

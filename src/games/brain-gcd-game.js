17885527dcf4e6b5753a0e0bb54317686b67fecd
VP <viktorpiros4@mail.ru>
2023-05-17 10:39:50 +0300

    fix comparison of user answer and correct answer comparison of number with string

--- src/games/brain-gcd-game.js
+++ src/games/brain-gcd-game.js
@@ -6,32 +6,25 @@ console.log('Welcome to the Brain Games!');
 const userName = getUserName();
 console.log(`Hello, ${userName}!`);
 
-const rightAnswerGCD = (a, b) => {
-  let resultA = a;
-  let resultB = b;
-  while (resultA !== resultB) {
-    if (resultA > resultB) {
-      resultA -= resultB;
-    } else if (resultA < resultB) {
-      resultB -= resultA;
-    }
+function rightAnswerGCD(a, b) {
+  if (!b) {
+    return a;
   }
-  return resultA;
-};
+  return rightAnswerGCD(b, a % b);
+}
 
 const gcdGame = () => {
   console.log('Find the greatest common divisor of given numbers.');
   const numb1 = getRandomIntInclusive();
   const numb2 = getRandomIntInclusive();
-  const question = `${numb1} ${numb2}`;
+  console.log(`Question: ${numb1} ${numb2}`);
   const correctAnswer = rightAnswerGCD(numb1, numb2);
   const usrAnswer = readlineSync.question('Your answer: ');
-  if (usrAnswer === correctAnswer) {
+  if (usrAnswer === String(correctAnswer)) {
     console.log('Correct');
   } else {
     console.log(`${usrAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
   }
-  return [question, String(correctAnswer)];
 };
 gcdGame();
 export default gcdGame;

b34e856ab56fdc8cdde4b061bbcc8a9aca387893
VP <viktorpiros4@mail.ru>
2023-05-23 14:37:50 +0300

    commit for finalizing games from home

--- src/games/brain-gcd-game.js
+++ src/games/brain-gcd-game.js
@@ -1,17 +1,17 @@
 import getRandomIntInclusive from '../utils/randomNumber.js';
 import startGame from '../utils/logic.js';
 
-function rightAnswerGCD(a, b) {
+function isGCD(a, b) {
   if (!b) {
     return a;
   }
-  return rightAnswerGCD(b, a % b);
+  return isGCD(b, a % b);
 }
 const description = ('Find the greatest common divisor of given numbers.');
 const game = () => {
     const numb1 = getRandomIntInclusive();
     const numb2 = getRandomIntInclusive();
-    const correctAnswer = rightAnswerGCD(numb1, numb2);
+    const correctAnswer = isGCD(numb1, numb2);
     return [`${numb1} ${numb2}`, correctAnswer];
 };
 const start = () => {
---
+++ src/games/brain-prime-game.js
@@ -0,0 +1,15 @@
+import getRandomIntInclusive from '../utils/randomNumber.js';
+import startGame from '../utils/logic.js';
+
+const description = ('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+const isPrime = (num) => {
+    for (let i = 2; i < num; i += 1) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  };
+
+const game = () => {
+
+}
